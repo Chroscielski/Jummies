6afca0a6ae89e5945480de21e7d60278c28f9ffa
DeeJayLysy <deejaylysy@gmail.com>
2015-01-23 20:35:16 +0100

    Uysy Next Next

---
+++ Assets/Scripts/HeroController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HeroController : MonoBehaviour
+{
+    [SerializeField]
+    MeleeColiderController meleeColiderController;
+
+    void Update()
+    {
+        if (Input.GetButtonDown("Jump"))
+        {
+            StartCoroutine(meleeColiderController.Attack());
+        }
+
+    }
+
+    public void TakeDamage()
+    {
+        Debug.Log("TOOK DAMAGE BIJACZ");
+    }
+}
---
+++ Assets/Scripts/MeleeColiderController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MeleeColiderController : MonoBehaviour
+{
+    public IEnumerator Attack()
+    {
+        Debug.Log("DZIALA1");
+        this.gameObject.collider.enabled = true;
+        yield return new WaitForSeconds(0.1f);
+        this.gameObject.collider.enabled = false;
+        Debug.Log("DZIALA2");
+    }
+
+    void OnTriggerEnter(Collider collider)
+    {
+        ((HeroController) collider.gameObject.GetComponent("HeroController")).TakeDamage();
+    }
+}

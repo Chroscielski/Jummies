16f97e1652f27b70954cc934ab868a15f2f2ef2c
Jacek Przemieniecki <jacek@przemieniecki.net>
2015-01-23 23:45:09 +0100

    Add armageddon prefab, add bomb prefab

---
+++ Assets/Scripts/Armageddon.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class Armageddon : MonoBehaviour
+{
+    public float MinX;
+    public float MaxX;
+    public float MinZ;
+    public float MaxZ;
+
+    public float BombSpawnHeight;
+
+    public int probability;
+
+    public GameObject BombPrefab;
+
+    void Update()
+    {
+        var rng = new System.Random();
+        if (rng.Next() < probability)
+        {
+            float x = Random.Range(MinX, MaxX);
+            float z = Random.Range(MinZ, MaxZ);
+
+            Instantiate(BombPrefab, new Vector3(x, BombSpawnHeight, z), Quaternion.identity);
+        }
+    }
+}

4d79bdf24a0282eaaebac080ece8166973969a3d
mathewp <mateusz.pusty@gmail.com>
2015-01-23 23:11:20 +0100

    BboyUysy Chuj Nie Raper

--- Assets/Scripts/GetInput.cs
+++ Assets/Scripts/GetInput.cs
@@ -6,6 +6,10 @@ using System.Reflection.Emit;
 public class GetInput : MonoBehaviour
 {
     public int numberOfPlayers = 2;
+
+    public HeroController P1;
+    public HeroController P2;
+
     /* 
      * Z nieznanych mi przyczyn Windows wykrywa kontroler 1 jako 2 a kontroler 2 jako 1
      * W Inpucie jest dlatego ustawione odwrotnie
@@ -16,8 +20,8 @@ public class GetInput : MonoBehaviour
     {
         Player1,
         Player2,
-        Player3,
-        Player4
+        //Player3,
+        //Player4
     }
 
     private void Awake()
@@ -36,24 +40,20 @@ public class GetInput : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.Joystick1Button0))
         {
-            /*Player1.Jump()*/
+            P1.Jump();
             Debug.Log("P1 Skacze");
         }
         if (Input.GetKeyDown(KeyCode.Joystick1Button2))
         {
-            /*Player1.Attack()*/
+            //Attack
             Debug.Log("P1 Atakuje");
         }
         float axis_X = Input.GetAxis("Joy 1 Axis X");
         float axis_Y = Input.GetAxis("Joy 1 Axis Y");
         float axis_3 = Input.GetAxis("Joy 1 Axis 3");
         float axis_4 = Input.GetAxis("Joy 1 Axis 4");
-
-        //Debug.Log("P1 Rotuje! Axis 3: " + axis_3.ToString() + "4: " + axis_4.ToString());
-        //Player1.rotate(axis_3, axis_4);
-
-        //Debug.Log("P1 Przesuwam! Axis X: " + axis_X.ToString() + "Y: " + axis_Y.ToString());
-        //Player1.rotate(axis_3, axis_4);
+        
+        P1.Move(axis_X, axis_Y);
 
     }
 
@@ -61,7 +61,7 @@ public class GetInput : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.Joystick2Button0))
         {
-            /*Player1.Jump()*/
+            P2.Jump();
             Debug.Log("P2 Skacze");
         }
         if (Input.GetKeyDown(KeyCode.Joystick2Button2))
@@ -73,16 +73,8 @@ public class GetInput : MonoBehaviour
         float axis_Y = Input.GetAxis("Joy 2 Axis Y");
         float axis_3 = Input.GetAxis("Joy 2 Axis 3");
         float axis_4 = Input.GetAxis("Joy 2 Axis 4");
-
-         Debug.Log(axis_X);
-         Debug.Log(axis_Y);
-         Debug.Log(axis_3);
-         Debug.Log(axis_4);
-        //Player1.rotate(axis_3, axis_4);
-
-            if (axis_X != 0 || axis_Y != 0) ;
-        //Debug.Log("P2 Przesuwam! Axis X: " + axis_X.ToString() + "Y: " + axis_Y.ToString());
-        //Player1.rotate(axis_3, axis_4);
+        
+        P2.Move(axis_X, axis_Y);
 
     }
 }

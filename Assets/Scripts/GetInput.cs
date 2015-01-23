1af8d14b80755150c235cb0d9eeef26cf8841c32
mathewp <mateusz.pusty@gmail.com>
2015-01-23 19:38:10 +0100

    Edited input

--- Assets/Scripts/GetInput.cs
+++ Assets/Scripts/GetInput.cs
@@ -1,15 +1,11 @@
-﻿using UnityEngine;
-using System.Collections;
+﻿using System;
+using UnityEngine;
 
-public class GetInput : MonoBehaviour {
+public class GetInput : MonoBehaviour
+{
+    void Update()
+    {
+        if (Input.GetButtonDown()) ;
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    }
 }

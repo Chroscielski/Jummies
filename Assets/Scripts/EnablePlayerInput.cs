95389913d15117cf17f58018c20609f54aeef0aa
mathewp <mateusz.pusty@gmail.com>
2015-01-24 19:56:37 +0100

    Dodano skrypt

---
+++ Assets/Scripts/EnablePlayerInput.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.EventSystems;
+
+public class EnablePlayerInput : MonoBehaviour
+{
+    private StandaloneInputModule input;
+
+    void Awake()
+    {
+        input = GetComponent<StandaloneInputModule>();
+
+        //GameManager.
+    }
+
+}

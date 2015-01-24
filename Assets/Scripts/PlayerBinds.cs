ae6c030fc00da849f554bd0b8bc92e49c5383772
mathewp <mateusz.pusty@gmail.com>
2015-01-24 01:09:01 +0100

    TeczusioweMaterialy

---
+++ Assets/Scripts/PlayerBinds.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using UnityEngine;
+using System.Collections.Generic;
+
+[Serializable]
+public class PlayerBinds
+{
+    [SerializeField] 
+    public List<KeyCode> button;
+
+    [SerializeField]
+    public List<float> axises;
+
+    public PlayerBinds()
+    {
+        button = new List<KeyCode>();
+        axises = new List<float>();
+    }
+}

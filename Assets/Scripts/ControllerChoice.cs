5bdac159c5d158d4720c42ff68d472c58120cf25
mathewp <mateusz.pusty@gmail.com>
2015-01-24 14:21:08 +0100

    Merge

--- Assets/Scripts/ControllerChoice.cs
+++ Assets/Scripts/ControllerChoice.cs
@@ -2,11 +2,18 @@
 
 public class ControllerChoice : MonoBehaviour 
 {
+   
+
     public void Start()
     {
         GameManager.SetControllerString(0, "Keyboard ");
         GameManager.SetControllerString(1, "Joy 1 ");
         GameManager.SetControllerString(2, "Joy 2 ");
         GameManager.SetControllerString(3, "Joy 3 ");
+    }
+
+    void getInput()
+    {
+        
     }
 }

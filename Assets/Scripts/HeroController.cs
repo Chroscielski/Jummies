e872315f4bb8e789335c1805815c767db3f93655
Jacek Przemieniecki <jacek@przemieniecki.net>
2015-01-25 08:19:48 +0100

    Set hit force

--- Assets/Scripts/HeroController.cs
+++ Assets/Scripts/HeroController.cs
@@ -13,11 +13,11 @@ public class HeroController : MonoBehaviour
     public bool canJump = true;
 
     public Animator AnimatorController;
-    public float ControllLossAfterHit = 6.0f;
+    private float ControllLossAfterHit = 6.0f;
 
     public int PlayerId;
 
-    private float controllModifier = 1.0f;
+    private float controlModifier = 1.0f;
 
     public void Rotate(float xAxis, float yAxis)
     {
@@ -26,7 +26,7 @@ public class HeroController : MonoBehaviour
 
     void Update()
     {
-        controllModifier = Mathf.MoveTowards(controllModifier, 1.0f, Time.deltaTime / ControllLossAfterHit);
+        controlModifier = Mathf.MoveTowards(controlModifier, 1.0f, Time.deltaTime / ControllLossAfterHit);
     }
 
     public void Move(float xAxis, float yAxis)
@@ -35,7 +35,7 @@ public class HeroController : MonoBehaviour
         Vector3 desiredVelocity = direction * movementSpeed;
         desiredVelocity.y = rigidbody.velocity.y;
         Vector3 currentVelocity = new Vector3(rigidbody.velocity.x, 0, rigidbody.velocity.z);
-        Vector3 setVelocity = Vector3.Lerp(currentVelocity, desiredVelocity, controllModifier);
+        Vector3 setVelocity = Vector3.Lerp(currentVelocity, desiredVelocity, controlModifier);
         setVelocity.y = rigidbody.velocity.y;
         rigidbody.velocity = setVelocity;
         AnimatorController.SetFloat("SpeedForward", Vector3.Dot(direction, transform.forward));
@@ -74,10 +74,8 @@ public class HeroController : MonoBehaviour
 
     public void TakeHit(Vector3 fromVector3)
     {
-        fromVector3 = new Vector3(fromVector3.x, 0.0f, fromVector3.z);
-        controllModifier = 0;
-        Vector3 myTmpVector3 = new Vector3(transform.position.x, 0, transform.position.z);
-        rigidbody.AddForce((myTmpVector3 - fromVector3) * 50.0f);
-        rigidbody.AddForce(Vector3.up * 100.0f);
+        fromVector3 = new Vector3(fromVector3.x, 0.0f, fromVector3.z).normalized;
+        controlModifier = 0;
+        rigidbody.AddExplosionForce(GameManager.HitForce, new Vector3(fromVector3.x, rigidbody.position.y, fromVector3.z), 0, GameManager.HitForce / 5);
     }
 }

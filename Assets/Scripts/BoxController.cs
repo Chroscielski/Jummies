877069c176e378532ed6e9b0c9c9b617ad96357c
DeeJayLysy <deejaylysy@gmail.com>
2015-01-25 10:36:30 +0100

    SuperBoxes Updated And Spawns Added

--- Assets/Scripts/BoxController.cs
+++ Assets/Scripts/BoxController.cs
@@ -5,6 +5,14 @@ public class BoxController : MonoBehaviour
 {
     [SerializeField] private GameObject explosionPrefab;
 
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "DeathZone")
+        {
+            DestroySelf();
+        }
+    }
+
     public void DestroySelf()
     {
         GameObject explosion = (GameObject)Instantiate(explosionPrefab, transform.position, Quaternion.identity);

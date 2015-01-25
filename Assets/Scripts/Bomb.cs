580cdd31a36071c929862690816ba481f1b569a7
Jacek Przemieniecki <jacek@przemieniecki.net>
2015-01-25 07:48:59 +0100

    Make bomb drop points visible

--- Assets/Scripts/Bomb.cs
+++ Assets/Scripts/Bomb.cs
@@ -3,10 +3,28 @@
 public class Bomb : MonoBehaviour
 {
     private const int playersLayer = 10;
+    private const int groundLayer = 8;
     public float explosionRadius;
     public float explosionForce;
 
     public GameObject ExplosionPrefab;
+    public GameObject CrossHairPrefab;
+    private GameObject _crossHair;
+
+    public Bomb(GameObject crossHair)
+    {
+        this._crossHair = crossHair;
+    }
+
+    void Start()
+    {
+        RaycastHit hitInfo;
+        bool hit = Physics.Raycast(transform.position, Vector3.down, out hitInfo, Mathf.Infinity, 1 << groundLayer);
+        if (hit)
+        {
+            _crossHair = (GameObject) Instantiate(CrossHairPrefab, hitInfo.point, Quaternion.identity);
+        }
+    }
 
     private void OnCollisionEnter(Collision col)
     {
@@ -19,5 +37,6 @@ public class Bomb : MonoBehaviour
         GameObject explosion = (GameObject)Instantiate(ExplosionPrefab, transform.position, Quaternion.identity);
         Destroy(explosion, 5);
         Destroy(gameObject);
+        Destroy(_crossHair);
     }
 }

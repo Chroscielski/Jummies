42889f1f81f37c572a08292de64c88394856e4d5
mathewp <mateusz.pusty@gmail.com>
2015-01-25 12:00:02 +0100

    Ładne menu

---
+++ Assets/Scripts/MenuCharacterController.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuCharacterController : MonoBehaviour
+{
+    private Animator anim;
+
+    void Awake()
+    {
+        anim = GetComponentInChildren<Animator>();
+    }
+
+    public void playChosenOneAnim()
+    {
+        anim.SetTrigger("IsChosen");
+    }
+}
